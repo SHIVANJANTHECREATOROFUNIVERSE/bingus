d174e9878d2863e380ca7cdd83327847698e9df4
SHIVANJANTHECREATOROFUNIVERSE <84670145+SHIVANJANTHECREATOROFUNIVERSE@users.noreply.github.com>
2025-05-19 12:20:04 +0530

    Add files via upload

---
+++ codelingo/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("CodeLang - Learn to Code")
+st.write("Welcome to your first chapter: Variables!")
+
+st.write("### Choose a lesson:")
+st.page_link("pages/Lesson1.py", label="👉 Start Lesson 1", icon="🎓")
